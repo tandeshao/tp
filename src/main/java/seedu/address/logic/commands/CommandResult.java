22a0a8e70ddce0e52b5faeecc1f213a28b8bcf3d
xpzmichael <xpzmichael@gmail.com>
2022-03-15 22:27:37 +0800

    fix checkstyle

--- src/main/java/seedu/address/logic/commands/CommandResult.java
+++ src/main/java/seedu/address/logic/commands/CommandResult.java
@@ -1,10 +1,11 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
 import seedu.address.logic.CommandRemark;
 
 import java.util.Objects;
 
-import static java.util.Objects.requireNonNull;
 
 /**
  * Represents the result of a command execution.
