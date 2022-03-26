e945f4a9df0fbd798c9d6b78e82b05262b276ad9
xpzmichael <xpzmichael@gmail.com>
2022-03-26 15:31:45 +0800

    Update CommandListTest.java

--- src/test/java/seedu/address/logic/commands/CommandListTest.java
+++ src/test/java/seedu/address/logic/commands/CommandListTest.java
@@ -1,12 +1,5 @@
 package seedu.address.logic.commands;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import seedu.address.logic.CommandList;
-
 public class CommandListTest {
-
-
 
 }
