5bb91cf20dc71dc9c516d8a8e953cf03fe0fcc92
xpzmichael <xpzmichael@gmail.com>
2022-03-27 13:04:34 +0800

    Update CommandListTest.java

--- src/test/java/seedu/address/logic/commands/CommandListTest.java
+++ src/test/java/seedu/address/logic/commands/CommandListTest.java
@@ -1,22 +1,20 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+
 import seedu.address.logic.CommandList;
-import seedu.address.logic.CommandRemark;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 
 public class CommandListTest {
 
     private final Model model = new ModelManager();
 
     @Test
-    public void getCurrentCommand_success(){
+    public void getCurrentCommand_success() {
         CommandList.getList().clearAllCommand();
         CommandList.getList().record("find n/ John1");
         CommandList.getList().record("find n/ John2");
