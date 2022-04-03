a1e4ad55b00422dad13461eae2adb48eca523f43
atmh <e0544426@u.nus.edu>
2022-04-03 22:59:19 +0800

    Add tests for Phone#equals()

--- src/test/java/seedu/address/model/person/PhoneTest.java
+++ src/test/java/seedu/address/model/person/PhoneTest.java
@@ -2,6 +2,8 @@ package seedu.address.model.person;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -40,5 +42,28 @@ public class PhoneTest {
         assertTrue(Phone.isValidPhone("911")); // exactly 3 numbers
         assertTrue(Phone.isValidPhone("93121534"));
         assertTrue(Phone.isValidPhone("124293842033123")); // long phone numbers
+    }
+
+    @Test
+    public void equals() {
+
+        Phone validPhoneAmy = new Phone(VALID_PHONE_AMY);
+        Phone validPhoneBob = new Phone(VALID_PHONE_BOB);
+
+        // same object -> returns true
+        assertTrue(validPhoneAmy.equals(validPhoneAmy));
+
+        // same values -> returns true
+        Phone validPhoneAmyCopy = new Phone(VALID_PHONE_AMY);
+        assertTrue(validPhoneAmy.equals(validPhoneAmyCopy));
+
+        // different types -> returns false
+        assertFalse(validPhoneAmy.equals(1));
+
+        // null -> returns false
+        assertFalse(validPhoneAmy.equals(null));
+
+        // different phone -> returns false
+        assertFalse(validPhoneAmy.equals(validPhoneBob));
     }
 }
