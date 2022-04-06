d43a7d90d4cf9eddad2ce9f1985eb449fb1237ab
xpzmichael <xpzmichael@gmail.com>
2022-04-06 13:51:15 +0800

    Update EmailTest.java

--- src/test/java/seedu/address/model/person/EmailTest.java
+++ src/test/java/seedu/address/model/person/EmailTest.java
@@ -62,7 +62,6 @@ public class EmailTest {
         assertFalse(Email.isValidEmail("peterjack@example.com-")); // domain name ends with a hyphen
         assertFalse(Email.isValidEmail("peterjack@example.c")); // top level domain has less than two chars
         assertFalse(Email.isValidEmail("peterjack@examplecom")); // no period
-        assertFalse(Email.isValidEmail("peterjack@comp.nus.edu.sg")); // many periods
         assertFalse(Email.isValidEmail(edgeEmail + "a")); // too long email
 
         // valid email
@@ -76,6 +75,7 @@ public class EmailTest {
         assertTrue(Email.isValidEmail("peter_jack@very-very-very-long-example.com")); // long domain name
         assertTrue(Email.isValidEmail("if.you.dream.it_you.can.do.it@example.com")); // long local part
         assertTrue(Email.isValidEmail("e1234567@u.nus.edu")); // more than one period in domain
+        assertTrue(Email.isValidEmail("peterjack@comp.nus.edu.sg")); // many periods
         assertTrue(Email.isValidEmail(edgeEmail)); // super long email
     }
 }
