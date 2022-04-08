41ab87a0f255cbe0cc6e4bfb096bae319572d2c6
xpzmichael <xpzmichael@gmail.com>
2022-04-08 16:32:11 +0800

    fix phone

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -11,15 +11,12 @@ public class Phone {
 
     public static final int PHONE_NUMBER_MAXIMUM = 100;
     public static final String MESSAGE_CONSTRAINTS =
-            "Phone numbers contains country code part and number part, country code should follow by a space. "
-                    + "country code part starts with a \"+\" and is optional. "
-                    + "Number part can only have numbers and spaces, it should contain at least 3 numbers. "
-                    + "Country code should not exceed 3 numbers. "
-                    + "The total length should not exceed "
-                    + PHONE_NUMBER_MAXIMUM + " characters"
-                    + "e.g. +65 12345678\n+123 888 777 666";
+            "Phone numbers should only contain numbers or white spaces."
+                    + " It may also contain '+' at the start, but must be followed by a number."
+                    + "  It should contain at least 3 numbers and cannot exceed "
+                    + PHONE_NUMBER_MAXIMUM + " characters";
     public static final String VALIDATION_REGEX = "(?=^.{3,"
-            + PHONE_NUMBER_MAXIMUM + "}$)(\\+\\d{1,3}\\s)?(\\s*\\d){3,}";
+            + PHONE_NUMBER_MAXIMUM + "}$)\\+?(\\d\\s*){3,}";
     public final String phone;
 
     /**
